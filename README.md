ec2feac910e98117dede1cd855000bd430a0259d
Paul Frazee <pfrazee@gmail.com>
2015-03-23 21:41:23 -0500

    add index.js

--- README.md
+++ README.md
@@ -4,8 +4,23 @@ Create/decorate an element which loads more content automatically when the scrol
 
 
 ```js
-// todo
+var infiniscroll = require('infiniscroll')
+var el = infiniscroll('div', {
+  fetchTop: function (cb) {
+    el.insertBefore(renderMoreTop(), el.firstChild)
+    cb()
+  },
+  fetchBottom: function (cb) {
+    el.appendChild(renderMoreBottom())
+    cb()
+  }
+})
 ```
+
+Notes:
+
+- `fetchTop` and `fetchBottom` are optional
+- the first param may be a tagname, hyperscript template string, or existing DOM element
 
 ## LICENSE
 
---
+++ index.js
@@ -0,0 +1,45 @@
+'use strict'
+var h = require('hyperscript')
+var multicb = require('multicb')
+
+
+module.exports = function (el, opts) {
+  opts = opts || {}
+  if (!opts)
+    throw new Error('Infiniscroll requires an `opts` object, please see https://github.com/pfraze/infiniscroll for usage')
+
+  if (typeof el == 'string')
+    el = h(el)
+  el.onscroll  = onscroll
+
+  var fetching = false
+  function onscroll (e) {
+    if (fetching)
+      return
+    if (el.offsetHeight + el.scrollTop >= el.scrollHeight) {
+      // hit bottom
+      if (typeof opts.fetchBottom == 'function') {
+        fetching = true
+        opts.fetchBottom(function (err) {
+          fetching = false
+          if (err)
+            console.error(err)
+        })
+      }
+    }
+    else if (el.scrollTop <= 1) {
+      // hit top
+      if (typeof opts.fetchTop == 'function'){
+        fetching = true
+        opts.fetchTop(function (err) {
+          fetching = false
+          el.scrollTop = 1          
+          if (err)
+            console.error(err)
+        })
+      }
+    }
+  }
+
+  return el
+}
